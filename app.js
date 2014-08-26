0aef72f0ed9d42517b5c2bdb5a21963c39261ece
JBarna <joel.tbarna@gmail.com>
2014-08-25 21:43:40 -0400

    Set up cookies.

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Session management
+var MongoClient = require('mongodb').MongoClient;
+var expressSession = require('express-session');
+var mongoStore = require('connect-mongo')({session: expressSession});
+
+
+//routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var gameroutes = require('./routes/gameroutes');
+
+//create app and mongo
+var mongo = new MongoClient();
 var app = express();
 
 // view engine setup
@@ -21,40 +31,50 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/game', gameroutes);
-app.use('/users', users);
+mongo.connect(process.env.MONGOLAB_URI || "mongodb://localhost/omok", function(err, db){
+    app.use(expressSession({secret: "wjsjsdflawe",
+                            cookie: {maxAge: 15* 60000},
+                            store: new mongoStore({ 
+                                'db': db,
+                                'collection': 'sessions'
+                            })
+                           }));
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    app.use('/', routes);
+    app.use('/game', gameroutes);
+    app.use('/users', users);
 
-/// error handlers
+    /// catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+    /// error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: {}
         });
     });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+}); // end session
 
 
 module.exports = app;
